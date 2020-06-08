e86c18feb58523a0f477f6837d75fc980a9f91c9
zhangdaiscott <zhangdaiscott@163.com>
2020-06-08 14:00:45 +0800

    1.0.0-beta 版本发布

--- common/service/config.service.js
+++ common/service/config.service.js
@@ -2,7 +2,7 @@ let BASE_URL = ''
 
 
 if (process.env.NODE_ENV == 'development') {
-    BASE_URL = 'http://boot.jeecg.org:8080/jeecg-boot' // 开发环境
+    BASE_URL = 'http://127.0.0.1:8080/jeecg-boot' // 开发环境
 } else {
 	BASE_URL = 'http://boot.jeecg.org:8080/jeecg-boot' // 生产环境
 }
--- common/service/service.js
+++ common/service/service.js
@@ -44,7 +44,6 @@ http.interceptor.request((config, cancel) => { /* 请求之前拦截器 */
     ...config.header,
      'X-Access-Token':getTokenStorage()
   }
- config.timeout = 9000
   /*
   if (!token) { // 如果token不存在，调用cancel 会取消本次请求，但是该函数的catch() 仍会执行
     cancel('token 不存在') // 接收一个参数，会传给catch((err) => {}) err.errMsg === 'token 不存在'
